da12f58953883e69d429ab1f29887aeea01da40b
J. Zebedee <zebedee@code.gripe>
2023-03-24 13:28:45 -0500

    Add demo

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿var random = new Random();
+while (true)
+{
+    Lazy<string> maybeBreaks = new Lazy<string>(() =>
+    {
+        var v = random.Next(100);
+        return v < 50 ? $"lazy-{v}" : throw new InvalidOperationException($"Failed lazy-{v}");
+    }, System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+    Parallel.For(0, 10, n =>
+    {
+        try
+        {
+            Console.WriteLine("{0} succeeded with value: {1}", n, maybeBreaks.Value);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("{0} got exception {1}", n, ex);
+        }
+    });
+    Console.ReadKey();
+    Console.Clear();
+}
